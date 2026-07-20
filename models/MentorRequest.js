163b174f63384457fddc9b776feef9536f4dfb08
yashtank01 <yashtank2006@gmail.com>
2026-07-20 23:04:15 +0530

    final commit

--- models/MentorRequest.js
+++ models/MentorRequest.js
@@ -1,19 +1,10 @@
 const mongoose = require('mongoose');
 
 const mentorRequestSchema = new mongoose.Schema({
-    mentorName: {
-        type: String,
-        required: true
-    },
-    // We can add the user's name/email later when you build a login system!
-    status: {
-        type: String,
-        default: 'Pending' // All new requests start as Pending
-    },
-    requestDate: {
-        type: Date,
-        default: Date.now
-    }
+    mentorName: { type: String, required: true },
+    userEmail: { type: String, required: true }, 
+    status: { type: String, default: 'Pending' },
+    requestDate: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('MentorRequest', mentorRequestSchema);
--- models/SavedIdea.js
+++ models/SavedIdea.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
 
 const savedIdeaSchema = new mongoose.Schema({
-    ideaName: {
-        type: String,
-        required: true
-    },
-    // We will link this to a specific user later!
-    savedAt: {
-        type: Date,
-        default: Date.now
-    }
+    ideaName: { type: String, required: true },
+    userEmail: { type: String, required: true }, 
+    savedAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('SavedIdea', savedIdeaSchema);
